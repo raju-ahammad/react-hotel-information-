bff1dd482aec443a159453304046e9624b65ee32
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-08 23:34:24 +0600

    create service state

--- src/Components/Services/Service.jsx
+++ src/Components/Services/Service.jsx
@@ -1,7 +1,37 @@
 import React, { Component } from 'react'
+import { FaCocktail } from "react-icons/fa"
 import Title from './Title'
 
+const serviceInfo = [
+    {
+        icon: <FaCocktail/>,
+        title: "Free Coktails",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        icon: <FaCocktail/>,
+        title: "Free Coktails",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        icon: <FaCocktail/>,
+        title: "Free Coktails",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        icon: <FaCocktail/>,
+        title: "Free Coktails",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    }
+]
+
+
 class Service extends Component {
+    state = {
+        services: [
+            
+        ]
+    }
     render() {
         return (
             <div>
