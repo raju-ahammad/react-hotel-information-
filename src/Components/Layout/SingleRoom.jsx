7dcc048765473c9f91cfb7bca335b123ae91a73f
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-09 20:07:58 +0600

    create single description

--- src/Components/Layout/SingleRoom.jsx
+++ src/Components/Layout/SingleRoom.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../../context';
+import defaultBcg from "../../images/defaultBcg.jpeg";
 
-const SingleRoom = () => {
-    return (
-        <div>
-            Single Rooms page
-        </div>
-    )
+
+export class SingleRoom extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props);
+        this.state = {
+          slug: this.props.match.params.slug,
+          defaultBcg: defaultBcg
+        };
+      }
+      static contextType = RoomContext
+
+    render() {
+        const {getRoom} = this.context
+        const room = getRoom(this.state.slug)
+        if(!room){
+            return(
+                <div className="error">
+                    <h3> no such room could be found...</h3>
+                    <Link to="/rooms" className="btn-primary">Back To Room</Link>
+                </div>
+            )
+        }
+        const {
+            name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+        } = room
+        const [main, ...defaultImages] = images;
+        console.log(defaultImages);
+        return (
+            <>
+            <section>
+                <p>{description}</p>
+            </section>
+            </>
+        )
+        
+    }
 }
 
 export default SingleRoom
