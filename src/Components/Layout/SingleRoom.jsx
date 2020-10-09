e2ab832d02a615715c37c35bc00985fa341a8363
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-09 22:17:38 +0600

    finsih single room

--- src/Components/Layout/SingleRoom.jsx
+++ src/Components/Layout/SingleRoom.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { RoomContext } from '../../context';
 import defaultBcg from "../../images/defaultBcg.jpeg";
+import StyledHero from '../Rooms/StyledHero';
+import Banner from './Banner';
 
 
 export class SingleRoom extends Component {
@@ -41,8 +43,38 @@ export class SingleRoom extends Component {
         console.log(defaultImages);
         return (
             <>
-            <section>
-                <p>{description}</p>
+            <StyledHero img= {images[0]|| this.state.defaultBcg} >
+            <Banner title={`${name} room`}>
+                <Link to="/rooms" className="btn-primary">back to room</Link>
+            </Banner>
+            </StyledHero>
+            <section className="single-room">
+                <div className="single-room-images">
+                {defaultImages.map((item, index) => (
+              <img key={index} src={item} alt={name} />
+            ))}
+                </div>
+                <div className="single-room-info">
+                <article className="info">
+                    <h3>info</h3>
+                    <h6>price : ${price}</h6>
+                    <h6>size : {size} SQFT</h6>
+                    <h6>
+                        max capacity :
+                        {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+                    </h6>
+                    <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+                    <h6>{breakfast && "free breakfast included"}</h6>
+                </article>
+                </div>
+            </section>
+            <section className="room-extras">
+            <h6>extras </h6>
+            <ul className="extras">
+                {extras.map((item, index) => (
+                <li key={index}>- {item}</li>
+                ))}
+            </ul>
             </section>
             </>
         )
