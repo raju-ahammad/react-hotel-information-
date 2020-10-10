3a11736a8e7daf00b8c5c428cdb584c0e615e8a5
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-11 00:54:05 +0600

    show filter type

--- src/Components/RoomContainer/RoomsFilter.jsx
+++ src/Components/RoomContainer/RoomsFilter.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import { RoomContext } from '../../context';
 import Title from '../Services/Title';
 
+const getUnique = (items, value) => {
+    return [new Set(items.map(item => item[value]))]
+}
 
 export const RoomsFilter = ({ rooms }) => {
     const context = useContext(RoomContext)
@@ -17,10 +20,32 @@ export const RoomsFilter = ({ rooms }) => {
         breakfast,
         pets
       } = context;
+      //get unique types
+      let types = getUnique(rooms, "type");
+      types = ["all", ...types];
+      types = types.map((item, index) => (
+        <option key={index} value={item}>
+          {item}
+        </option>
+      ));
 
     return (
         <secttion className="filter-container">
             <Title title="Search Rooms"/>
+            <form className="filter-form">
+            <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            onChange={handleChange}
+            className="form-control"
+            value={type}
+          >
+            {types}
+          </select>
+        </div>
+            </form>
         </secttion>
     )
 }
